5886fd5dd7f66c0af0f4c627aa2f66b7c52cf70e
Rob Lowe <robert.lowe@digital.justice.gov.uk>
2023-07-25 09:50:15 +0100

    Add plugin details

---
+++ feed-fixer.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Plugin name: Feed Fixer
+ * Plugin URI:  https://github.com/ministryofjustice/feed-fixer
+ * Description: Parses feed into Wordpress
+ * Version:     1.0.0
+ * Author:      Ministry of Justice - Adam Brown, Malcolm Butler & Robert Lowe
+ * Text domain: feed-fixer
+ * Author URI:  https://github.com/ministryofjustice
+ * License:     MIT Licence
+ * License URI: https://opensource.org/licenses/MIT
+ * Copyright:   Crown Copyright (c) Ministry of Justice
+ **/
+
+
+
